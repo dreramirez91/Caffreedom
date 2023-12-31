28c82d176a878a0f0e6d80a7f80d8bf8075bdddc
dreramirez91 <dreramirez91@gmail.com>
2023-12-31 03:26:29 -0600

    Ignore case

---
+++ Native/components/Footer.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { useFonts, Lora_400Regular_Italic } from "@expo-google-fonts/lora";
+import {
+  MaterialCommunityIcons,
+  Octicons,
+  AntDesign,
+  FontAwesome,
+} from "@expo/vector-icons";
+import { Link } from "expo-router";
+
+export default function Footer() {
+  let [fontsLoaded] = useFonts({
+    Lora_400Regular_Italic,
+  });
+
+  if (fontsLoaded) {
+    return (
+      <View style={styles.footerContainer}>
+        <Link style={styles.footerText} href="/home">
+          <MaterialCommunityIcons
+            name="home-outline"
+            size={32}
+            color="rgba(242, 255, 99, 1)"
+          />
+        </Link>
+        <Link style={styles.footerText} href="/calculator">
+          <AntDesign
+            name="calculator"
+            size={32}
+            color="rgba(242, 255, 99, 1)"
+          />
+        </Link>
+        <Link style={styles.footerText} href="/table">
+          <FontAwesome name="table" size={32} color="rgba(242, 255, 99, 1)" />
+        </Link>
+        <Link style={styles.footerText} href="/graph">
+          <Octicons name="graph" size={32} color="rgba(242, 255, 99, 1)" />
+        </Link>
+      </View>
+    );
+  } else {
+    return null;
+  }
+}
+
+const styles = StyleSheet.create({
+  footerContainer: {
+    bottom: 0,
+    position: "fixed",
+    backgroundColor: "rgba(157, 108, 255, 0.70)",
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexDirection: "row",
+    paddingTop: 4,
+    paddingBottom: 2,
+    borderTopWidth: 2,
+    borderColor: "rgba(94, 25, 121, 1)",
+  },
+  footerText: {
+    color: "rgba(242, 255, 99, 1)",
+    fontFamily: "Lora_400Regular_Italic",
+    fontSize: 18,
+  },
+});
