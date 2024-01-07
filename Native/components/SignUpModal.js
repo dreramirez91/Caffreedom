fe7659e09036110cabcae5fca27c36c80f94792f
dreramirez91 <dreramirez91@gmail.com>
2024-01-07 14:39:41 -0600

    Clear confirm password if signup successful

--- Native/components/SignUpModal.js
+++ Native/components/SignUpModal.js
@@ -33,6 +33,7 @@ export default function SignUpModal({ signUpModalVisible, setSignUpModalVisible,
       save("token", token);
       setUsername("");
       setPassword("");
+      setConfirmPassword(" ");
       setSignUpModalVisible(false);
       setSignUpSuccessful(true);
     } else {
