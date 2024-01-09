99f0a8b6074628410631f624fb0ee9a13064aca2
dreramirez91 <dreramirez91@gmail.com>
2024-01-09 12:09:45 -0600

    Fix accordion where all would open at once

--- Native/app/info.js
+++ Native/app/info.js
@@ -11,54 +11,56 @@ export default function Info() {
   return (
     <ScrollView style={styles.infoContainer}>
       <List.Section titleStyle={styles.sectionTitle} title="About Us">
-        <List.Accordion titleStyle={styles.accordianTitle} title="About Caffreedom" left={(props) => <List.Icon {...props} icon="" />} expanded={expanded} onPress={handlePress}>
-          <List.Item
-            descriptionNumberOfLines={99}
-            descriptionStyle={styles.description}
-            description={
-              <>
-                Caffreedom is a caffeine intake tracker designed to help users monitor and manage their caffeine consumption. Our app allows users to record their beverage intake, including types of drinks and their respective caffeine content.{"\n\n"}
-                <Text style={styles.subSectionHeader}>Acknowledgment</Text>
-                {"\n\n"}We gratefully acknowledge the use of caffeine content information provided by Caffeine Informer (https://www.caffeineinformer.com/) to facilitate accurate tracking within our app.{"\n\n"}
-                Caffreedom is committed to safeguarding user privacy and ensuring a seamless caffeine intake tracking experience.{"\n"}
-              </>
-            }
-          />
-        </List.Accordion>
-        <List.Accordion titleStyle={styles.accordianTitle} title="Privacy Policy" left={(props) => <List.Icon {...props} icon="" />} expanded={expanded} onPress={handlePress}>
-          <List.Item
-            descriptionNumberOfLines={99}
-            descriptionStyle={styles.description}
-            description={
-              <>
-                <Text style={styles.subSectionHeader}>Privacy Policy for Caffreedom</Text>
-                {"\n\n"}This Privacy Policy governs the manner in which Caffreedom collects, uses, maintains, and discloses information collected from users of the Caffreedom mobile application.{"\n\n"}
-                <Text style={styles.subSectionHeader}>Information Collection and Use</Text>
-                {"\n\n"}Caffreedom collects and uses personal information for the sole purpose of providing a caffeine intake tracking service. The only information collected is user-provided data: When creating an account, users may input their caffeine intake details, such as beverage types, intake dates, and caffeine content.{"\n\n"}
-                <Text style={styles.subSectionHeader}>Data Sharing and Security</Text>
-                {"\n\n"}
-                Caffreedom does not share any user data with third parties. All user-provided information is securely stored and used exclusively for the intended purpose of tracking caffeine intake.
-                {"\n\n"}
-                <Text style={styles.subSectionHeader}>Data Accuracy</Text>
-                {"\n\n"}
-                Caffreedom utilizes caffeine content information from Caffeine Informer (https://www.caffeineinformer.com/) for accurate tracking of caffeine intake. Users' reliance on this data is subject to the accuracy of the source.
-                {"\n\n"}
-                <Text style={styles.subSectionHeader}>Third-Party Links</Text>
-                {"\n\n"}
-                Caffreedom may contain links to external sites, including the Caffeine Informer website. Users are encouraged to review the privacy policies of these external sites, as Caffreedom holds no responsibility for the content and practices of these sites.
-                {"\n\n"}
-                <Text style={styles.subSectionHeader}>Changes to This Privacy Policy</Text>
-                {"\n\n"}
-                Caffreedom reserves the right to update this privacy policy at any time. Users will be notified of any changes by revising the "last updated" date at the bottom of this page.
-                {"\n\n"}
-                <Text style={styles.subSectionHeader}>Contact Information</Text>
-                {"\n\n"}
-                If you have any questions or concerns about this Privacy Policy, please contact us at caffreedom@gmail.com.
-                {"\n\n"}
-              </>
-            }
-          />
-        </List.Accordion>
+        <List.AccordionGroup>
+          <List.Accordion id="1" titleStyle={styles.accordianTitle} title="About Caffreedom" left={(props) => <List.Icon {...props} icon="" />} expanded={expanded} onPress={handlePress}>
+            <List.Item
+              descriptionNumberOfLines={99}
+              descriptionStyle={styles.description}
+              description={
+                <>
+                  Caffreedom is a caffeine intake tracker designed to help users monitor and manage their caffeine consumption. Our app allows users to record their beverage intake, including types of drinks and their respective caffeine content.{"\n\n"}
+                  <Text style={styles.subSectionHeader}>Acknowledgment</Text>
+                  {"\n\n"}We gratefully acknowledge the use of caffeine content information provided by Caffeine Informer (https://www.caffeineinformer.com/) to facilitate accurate tracking within our app.{"\n\n"}
+                  Caffreedom is committed to safeguarding user privacy and ensuring a seamless caffeine intake tracking experience.{"\n"}
+                </>
+              }
+            />
+          </List.Accordion>
+          <List.Accordion id="2" titleStyle={styles.accordianTitle} title="Privacy Policy" left={(props) => <List.Icon {...props} icon="" />} expanded={expanded} onPress={handlePress}>
+            <List.Item
+              descriptionNumberOfLines={99}
+              descriptionStyle={styles.description}
+              description={
+                <>
+                  <Text style={styles.subSectionHeader}>Privacy Policy for Caffreedom</Text>
+                  {"\n\n"}This Privacy Policy governs the manner in which Caffreedom collects, uses, maintains, and discloses information collected from users of the Caffreedom mobile application.{"\n\n"}
+                  <Text style={styles.subSectionHeader}>Information Collection and Use</Text>
+                  {"\n\n"}Caffreedom collects and uses personal information for the sole purpose of providing a caffeine intake tracking service. The only information collected is user-provided data: When creating an account, users may input their caffeine intake details, such as beverage types, intake dates, and caffeine content.{"\n\n"}
+                  <Text style={styles.subSectionHeader}>Data Sharing and Security</Text>
+                  {"\n\n"}
+                  Caffreedom does not share any user data with third parties. All user-provided information is securely stored and used exclusively for the intended purpose of tracking caffeine intake.
+                  {"\n\n"}
+                  <Text style={styles.subSectionHeader}>Data Accuracy</Text>
+                  {"\n\n"}
+                  Caffreedom utilizes caffeine content information from Caffeine Informer (https://www.caffeineinformer.com/) for accurate tracking of caffeine intake. Users' reliance on this data is subject to the accuracy of the source.
+                  {"\n\n"}
+                  <Text style={styles.subSectionHeader}>Third-Party Links</Text>
+                  {"\n\n"}
+                  Caffreedom may contain links to external sites, including the Caffeine Informer website. Users are encouraged to review the privacy policies of these external sites, as Caffreedom holds no responsibility for the content and practices of these sites.
+                  {"\n\n"}
+                  <Text style={styles.subSectionHeader}>Changes to This Privacy Policy</Text>
+                  {"\n\n"}
+                  Caffreedom reserves the right to update this privacy policy at any time. Users will be notified of any changes by revising the "last updated" date at the bottom of this page.
+                  {"\n\n"}
+                  <Text style={styles.subSectionHeader}>Contact Information</Text>
+                  {"\n\n"}
+                  If you have any questions or concerns about this Privacy Policy, please contact us at caffreedom@gmail.com.
+                  {"\n\n"}
+                </>
+              }
+            />
+          </List.Accordion>
+        </List.AccordionGroup>
       </List.Section>
     </ScrollView>
   );
