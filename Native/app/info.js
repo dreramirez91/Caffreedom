1bcbc49084a0d90ad0c1746d4ae08bbff36c2299
dreramirez91 <dreramirez91@gmail.com>
2024-01-10 12:17:58 -0600

    Merge delete confirmation modal and endpoints

--- Native/app/info.js
+++ Native/app/info.js
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import { StyleSheet, Text, View, Image, Pressable, TextInput } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import { Button, List } from "react-native-paper";
+import { Modal, Portal, List, Button } from "react-native-paper";
 import * as SecureStore from "expo-secure-store";
-import { useRouter, useFocusEffect } from "expo-router";
+import { useRouter } from "expo-router";
+import { Row } from "react-native-table-component";
 
 export default function Info() {
-  const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+  const apiUrl = "http://192.168.86.102:8000";
   const [expanded, setExpanded] = React.useState(true);
   const handlePress = () => setExpanded(!expanded);
   const router = useRouter();
+  const [visible, setVisible] = useState(false);
+  const [username, setUsername] = useState("");
+  const [error, setError] = useState("");
+  const showModal = () => setVisible(true);
+  const hideModal = () => {
+    setUsername("");
+    setError("");
+    setVisible(false);
+  };
 
   async function deleteUser(key) {
-    console.log("SUP");
     try {
+      data = { username: username.toLowerCase() };
       let result = await SecureStore.getItemAsync(key);
       if (result) {
         const fetchConfig = {
@@ -22,14 +32,16 @@ export default function Info() {
             "Content-type": "application/json",
             Authorization: result,
           },
+          body: JSON.stringify(data),
         };
         const response = await fetch(`${apiUrl}/users/delete/`, fetchConfig);
+        const data = await response.json();
         if (response.ok) {
-          const data = await response.json();
           SecureStore.deleteItemAsync("token");
           router.replace("/home");
           return;
         } else {
+          setError(data.error);
           console.log("Delete failed");
         }
       } else {
@@ -102,9 +114,29 @@ export default function Info() {
               descriptionNumberOfLines={99}
               description={
                 <>
-                  <Pressable onPress={() => deleteUser("token")}>
+                  <Pressable onPress={() => showModal(true)}>
                     <Text style={styles.deleteYourAccount}>Click here to delete your account and all of its associated records.</Text>
                   </Pressable>
+                  <View style={styles.centeredView}>
+                    <Portal>
+                      <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={styles.containerStyle}>
+                        <Text style={styles.modalHeader}>Are you sure?</Text>
+
+                        <View>
+                          <TextInput style={styles.input} onChangeText={setUsername} placeholder="Enter your username to confirm" value={username}></TextInput>
+                          {error ? <Text style={styles.errorText}>{error}</Text> : null}
+                        </View>
+                        <View style={styles.buttons}>
+                          <Button onPress={() => deleteUser("token")} mode="contained" buttonColor="rgba(94, 65, 153, 1)">
+                            Delete my Account
+                          </Button>
+                          <Button onPress={() => hideModal()} mode="contained" buttonColor="rgba(94, 65, 153, 1)">
+                            Cancel
+                          </Button>
+                        </View>
+                      </Modal>
+                    </Portal>
+                  </View>
                 </>
               }
             />
@@ -132,11 +164,55 @@ const styles = StyleSheet.create({
   accordianTitle: { fontSize: 24, fontFamily: "CrimsonPro_400Regular", color: "rgba(157, 108, 255, 1)" },
   description: { fontSize: 18, fontFamily: "CrimsonPro_400Regular", color: "rgba(242, 255, 99, 1)" },
   deleteYourAccount: {
-    color: "rgba(255, 99, 99, 1)",
+    color: "rgba(242, 255, 99, 1)",
     textShadowColor: "rgba(0, 0, 0, 0.5)",
+    textDecorationLine: "underline",
     textShadowOffset: { width: -1, height: 1 },
     textShadowRadius: 10,
     fontFamily: "CrimsonPro_400Regular",
     fontSize: 22,
+  },
+  modalHeader: {
+    textAlign: "center",
+    color: "rgba(242, 255, 99, 1)",
+    fontFamily: "CrimsonPro_400Regular",
+    fontSize: 22,
+    padding: 10,
+  },
+  input: {
+    color: "black",
+    fontFamily: "CrimsonPro_400Regular",
+    borderRadius: 8,
+    borderColor: "rgba(242, 255, 99, 1)",
+    backgroundColor: "white",
+    borderWidth: 1,
+    padding: 10,
+    margin: 10,
+    fontSize: 18,
+  },
+  containerStyle: {
+    margin: 20,
+    backgroundColor: "rgba(157, 108, 255, 1)",
+    borderRadius: 20,
+    padding: 35,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    padding: 10,
+  },
+  errorText: {
+    color: "rgba(242, 255, 99, 1)",
+    fontFamily: "CrimsonPro_400Regular",
+    textAlign: "center",
+    fontSize: 18,
   },
 });
