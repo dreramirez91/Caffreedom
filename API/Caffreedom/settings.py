16d222d42c2b737fa466f9aeaa3a8a5b6b6b2f9a
dreramirez91 <dreramirez91@gmail.com>
2024-01-17 23:25:08 -0600

    Change allowed hosts to wildcard

--- API/Caffreedom/settings.py
+++ API/Caffreedom/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-#2wmdbu-*c^y9dbkhoz^pc_bct5452@7-%y*v+on7rz*$0j=o)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.86.102", ".vercel.app", "*"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
